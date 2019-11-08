e83e9454213884fc3ee2aa73c70d4c25e3f4e437
kuba1pie <kuba1pie@gmail.com>
2019-11-08 16:33:08 +0100

    portfolio filter script

--- src/script/script.js
+++ src/script/script.js
@@ -26,17 +26,45 @@ function navMenuShow(){
 
 function portfolio(){
     let wrapperMenu = document.getElementById("portfolioMenu")
-    addItem(wrapperMenu, 'All', 'all')
+    addItem(wrapperMenu, 'All', 'all', "all", filter)
     let i
     for (i = 0; i<portfolioData.categories.length; i++){
         let catName = portfolioData.categories[i].name
-        addItem(wrapperMenu, catName, "category_"+catName, "category")
+        addItem(wrapperMenu, catName, "category_"+i, "category", filter)
     } 
+    filter()
 }
-function addItem(wrapper, item, id, cla){
+function addItem(wrapper, item, id, cla, filter){
     let el = document.createElement("div")
     el.classList.add(cla)
     el.id = id
     el.innerHTML = item
+    el.addEventListener("click", filter);
     wrapper.appendChild(el)
+}
+function filter(){
+    let wrapper=document.getElementById("portfolioWrapper")
+    if (this == null || this.id == 'all'){
+        let i
+        let max=3
+        for (i=0; i<max; i++){
+            print(wrapper, i)
+        }
+    } else {
+        wrapper.innerHTML=""
+        let category=this.id.replace("category_", "")
+        print(wrapper, category)
+    }
+}
+function print(wrapper, category){
+    let i
+    for (i = 0; i<portfolioData.categories[category].items.length; i++){
+        let itemName = portfolioData.categories[category].items[i].name
+        let itemPhoto = portfolioData.categories[category].items[i].photo
+        let itemDesc = portfolioData.categories[category].items[i].description
+        let divHtml="<h3>"+itemName+"</h3><img src=src/style/"+(i+1)+".png alt='"+itemName+"'><p>"+itemDesc+"</p>"
+        let id = ''
+        let cla = "item"
+        addItem(wrapper, divHtml, id, cla)
+    }
 }
